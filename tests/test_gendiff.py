111513cfd3c79a1e5e36811eff5e33c7b6e1e770
pestrukha <sturbauen@gmail.com>
2024-06-01 23:50:18 +0400

    add fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,8 +3,8 @@ from gendiff import generate_diff
 
 
 def test_gendiff():
-    file_1 = 'tests/fixtures/file1.json'
-    file_2 = 'tests/fixtures/file2.json'
+    file_1 = 'tests/fixtures/1.json'
+    file_2 = 'tests/fixtures/2.json'
     result = 'tests/fixtures/result.txt'
     diff = print(generate_diff(file_1, file_2))
     assert diff == print(result)
