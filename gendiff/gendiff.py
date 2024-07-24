c0803b614178a41de0eecc3d557a65540ec51887
pestrukha <sturbauen@gmail.com>
2024-07-25 03:10:28 +0400

    delete str(file_1)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,8 +6,6 @@ from gendiff.formatters.json import convert_to_json
 
 
 def generate_diff(file_1, file_2, format='stylish'):
-    file_1 = str(file_1)
-    file_2 = str(file_2)
     dict_1 = parse_data(file_1)
     dict_2 = parse_data(file_2)
     diff = build_diff(dict_1, dict_2)
