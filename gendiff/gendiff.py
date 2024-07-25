77339d1564af4488b57b1d1e10fd4a9c992126ea
pestrukha <sturbauen@gmail.com>
2024-07-25 05:11:03 +0400

    add get_data

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,10 +1,10 @@
-from gendiff.parser import parse_data
+from gendiff.parser import get_data
 from gendiff.build_diff import build_diff
 from gendiff.formatting import format_diff
 
 
 def generate_diff(file_1, file_2, format='stylish'):
-    dict_1 = parse_data(file_1)
-    dict_2 = parse_data(file_2)
+    dict_1 = get_data(file_1)
+    dict_2 = get_data(file_2)
     diff = build_diff(dict_1, dict_2)
     return format_diff(diff, format)
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,13 +1,18 @@
 import json
 import yaml
+import os
 
 
-def parse_data(file_path):
-    file_path = str(file_path)
+def parse_data(file, extension):
+    if extension == '.json':
+        return json.loads(file)
+    elif extension in ['.yml', '.yaml']:
+        return yaml.load(file, Loader=yaml.Loader)
+    else:
+        raise ValueError(f'Unsupported file format: {extension}')
+
+
+def get_data(file_path):
+    extension = os.path.splitext(file_path)[1].lower()
     with open(file_path) as file:
-        if file_path.endswith('.json'):
-            return json.load(file)
-        elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
-            return yaml.load(file, Loader=yaml.Loader)
-        else:
-            raise ValueError('Unsupported file format: {file_path}')
+        return parse_data(file.read(), extension)
