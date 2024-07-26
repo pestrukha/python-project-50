0c95bbd95dbfc0de056c7ae0da3f1b5da1cc8376
pestrukha <sturbauen@gmail.com>
2024-07-26 04:27:38 +0400

    fix linter errors

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -6,6 +6,7 @@ import os
 class ExtensionError(Exception):
     pass
 
+
 def parse_data(file, extension):
     if extension == '.json':
         return json.loads(file)
@@ -13,6 +14,7 @@ def parse_data(file, extension):
         return yaml.load(file, Loader=yaml.Loader)
     else:
         raise ExtensionError(f'Unsupported file format: {extension}')
+
 
 def get_data(file_path):
     extension = os.path.splitext(file_path)[1].lower()
