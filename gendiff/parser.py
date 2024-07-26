361698d6e1881bd6d433344e2cc0043b271b0945
pestrukha <sturbauen@gmail.com>
2024-07-26 04:24:33 +0400

    fix ValueError

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,14 +3,16 @@ import yaml
 import os
 
 
+class ExtensionError(Exception):
+    pass
+
 def parse_data(file, extension):
     if extension == '.json':
         return json.loads(file)
     elif extension in ['.yml', '.yaml']:
         return yaml.load(file, Loader=yaml.Loader)
     else:
-        raise ValueError(f'Unsupported file format: {extension}')
-
+        raise ExtensionError(f'Unsupported file format: {extension}')
 
 def get_data(file_path):
     extension = os.path.splitext(file_path)[1].lower()
