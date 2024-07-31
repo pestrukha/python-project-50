a5bd6d46063ad95845534c3e6255bf8c05ad95f9
pestrukha <sturbauen@gmail.com>
2024-07-31 12:51:16 +0400

    fix plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -10,40 +10,34 @@ def format_value(value):
     return str(value)
 
 
-def handle_added(full_path, new_value):
-    formatted_value = format_value(new_value)
-    return f"Property '{full_path}' was added with value: {formatted_value}"
-
-
-def handle_removed(full_path):
-    return f"Property '{full_path}' was removed"
-
-
-def handle_updated(full_path, old_value, new_value):
-    formatted_old_value = format_value(old_value)
-    formatted_new_value = format_value(new_value)
-    return (f"Property '{full_path}' was updated. "
-            f"From {formatted_old_value} to {formatted_new_value}")
-
-
-def generate_messages(diff, path=""):
+def generate_messages(diff, path=''):
     messages = []
     for item in diff:
         key = item['key']
-        full_path = f"{path}.{key}" if path else key
+        full_path = f'{path}.{key}' if path else key
         status = item['status']
 
         if status == 'nested':
             nested_diff = item['nested']
             messages.extend(generate_messages(nested_diff, full_path))
         elif status == 'added':
-            messages.append(handle_added(full_path, item['new_value']))
+            formatted_value = format_value(item['new_value'])
+            message = (
+                f"Property '{full_path}' was added with value: "
+                f"{formatted_value}"
+            )
+            messages.append(message)
         elif status == 'removed':
-            messages.append(handle_removed(full_path))
+            message = f"Property '{full_path}' was removed"
+            messages.append(message)
         elif status == 'updated':
-            messages.append(handle_updated(
-                full_path, item['old_value'], item['new_value']
-            ))
+            formatted_old_value = format_value(item['old_value'])
+            formatted_new_value = format_value(item['new_value'])
+            message = (
+                f"Property '{full_path}' was updated. From "
+                f"{formatted_old_value} to {formatted_new_value}"
+            )
+            messages.append(message)
     return messages
 
 
